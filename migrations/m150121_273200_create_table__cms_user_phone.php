20597b57b5dd34c96dfc847663136070df284094
Semenov <semenov@skeeks.com>
2015-01-27 13:33:15 +0300

    Изминенеия по пользователям

--- migrations/m150121_273200_create_table__cms_user_phone.php
+++ migrations/m150121_273200_create_table__cms_user_phone.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * m150121_193200_create_table__cms_user_email
+ * m150121_273200_create_table__cms_user_phone
  *
  * @author Semenov Alexander <semenov@skeeks.com>
  * @link http://skeeks.com/
  * @copyright 2010-2014 SkeekS (Sx)
- * @date 21.01.2015
+ * @date 27.01.2015
  * @since 1.0.0
  */
 use yii\db\Schema;
 use yii\db\Migration;
 
 /**
- * Class m150121_193200_create_table__cms_user_email
+ * Class m150121_273200_create_table__cms_user_phone
  */
-class m150121_193200_create_table__cms_user_email extends Migration
+class m150121_273200_create_table__cms_user_phone extends Migration
 {
     public function up()
     {
-        $tableExist = $this->db->getTableSchema("{{%cms_user_email}}", true);
+        $tableExist = $this->db->getTableSchema("{{%cms_user_phone}}", true);
         if ($tableExist)
         {
             return true;
@@ -30,7 +30,7 @@ class m150121_193200_create_table__cms_user_email extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
         }
 
-        $this->createTable("{{%cms_user_email}}", [
+        $this->createTable("{{%cms_user_phone}}", [
             'id'                    => Schema::TYPE_PK,
             'user_id'               => Schema::TYPE_INTEGER . ' NOT NULL',
             'value'                 => Schema::TYPE_STRING. '(255) NOT NULL',
@@ -40,23 +40,22 @@ class m150121_193200_create_table__cms_user_email extends Migration
             'updated_at'            => Schema::TYPE_INTEGER . ' NULL',
         ], $tableOptions);
 
-        $this->execute("ALTER TABLE {{%cms_user_email}} ADD INDEX(approved_key);");
-        $this->execute("ALTER TABLE {{%cms_user_email}} ADD INDEX(approved);");
-        $this->execute("ALTER TABLE {{%cms_user_email}} ADD INDEX(user_id);");
-        $this->execute("ALTER TABLE {{%cms_user_email}} ADD INDEX(created_at);");
-        $this->execute("ALTER TABLE {{%cms_user_email}} ADD INDEX(updated_at);");
-        $this->execute("ALTER TABLE {{%cms_user_email}} ADD UNIQUE(value);");
+        $this->execute("ALTER TABLE {{%cms_user_phone}} ADD INDEX(approved_key);");
+        $this->execute("ALTER TABLE {{%cms_user_phone}} ADD INDEX(approved);");
+        $this->execute("ALTER TABLE {{%cms_user_phone}} ADD INDEX(user_id);");
+        $this->execute("ALTER TABLE {{%cms_user_phone}} ADD INDEX(created_at);");
+        $this->execute("ALTER TABLE {{%cms_user_phone}} ADD INDEX(updated_at);");
+        $this->execute("ALTER TABLE {{%cms_user_phone}} ADD UNIQUE(value);");
 
         $this->addForeignKey(
-            'cms_user_email_user_id', "{{%cms_user_email}}",
+            'cms_user_phone_user_id', "{{%cms_user_phone}}",
             'user_id', '{{%cms_user}}', 'id', 'CASCADE', 'CASCADE'
         );
-
     }
 
     public function down()
     {
-        $this->dropForeignKey("cms_user_email_user_id", "{{%cms_user_email}}");
-        $this->dropTable("{{%cms_user_email}}");
+        $this->dropForeignKey("cms_user_phone_user_id", "{{%cms_user_phone}}");
+        $this->dropTable("{{%cms_user_phone}}");
     }
 }
--- models/user/UserEmail.php
+++ models/user/UserEmail.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * UserEmail
@@ -6,17 +5,18 @@
  * @author Semenov Alexander <semenov@skeeks.com>
  * @link http://skeeks.com/
  * @copyright 2010-2014 SkeekS (Sx)
- * @date 21.01.2015
+ * @date 27.01.2015
  * @since 1.0.0
  */
-namespace skeeks\cms\models;
+namespace skeeks\cms\models\user;
 
-use skeeks\cms\base\db\ActiveRecord;
 use Yii;
+use yii\behaviors\TimestampBehavior;
+use \yii\db\ActiveRecord;
 
 /**
  * Class UserEmail
- * @package skeeks\cms\models
+ * @package skeeks\cms\models\user
  */
 class UserEmail extends ActiveRecord
 {
@@ -31,11 +31,21 @@ class UserEmail extends ActiveRecord
     /**
      * @inheritdoc
      */
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            TimestampBehavior::className()
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function rules()
     {
         return [
-            [['value'], 'required'],
-            [['user_id', 'created_at', 'updated_at', 'approved', 'main'], 'integer'],
+            [['user_id', 'value'], 'required'],
+            [['user_id', 'created_at', 'updated_at', 'approved'], 'integer'],
             [['value', 'approved_key'], 'string'],
         ];
     }
@@ -47,11 +57,10 @@ class UserEmail extends ActiveRecord
     {
         return [
             'id' => Yii::t('app', 'ID'),
-            'user_id' => Yii::t('app', 'User ID'),
-            'value' => Yii::t('app', 'Value'),
-            'approved_key' => Yii::t('app', 'Ключ подтверждения'),
-            'approved' => Yii::t('app', 'Подтвержденный?'),
-            'main' => Yii::t('app', 'Главный'),
+            'user_id' => Yii::t('app', 'Пользователь'),
+            'provider' => Yii::t('app', 'Provider'),
+            'provider_identifier' => Yii::t('app', 'Provider Identifier'),
+            'provider_data' => Yii::t('app', 'Provider Data'),
             'created_at' => Yii::t('app', 'Created At'),
             'updated_at' => Yii::t('app', 'Updated At'),
         ];
@@ -60,7 +69,7 @@ class UserEmail extends ActiveRecord
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function fetchUser()
+    public function getUser()
     {
         return $this->hasOne(User::className(), ['id' => 'user_id']);
     }
