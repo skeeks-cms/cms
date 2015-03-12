806c0569ba3602d2b7df78523d8f5121e2389501
Semenov <semenov@skeeks.com>
2015-03-12 15:14:13 +0300

    ssh console в админке

---
+++ modules/admin/models/forms/SshConsoleForm.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 12.03.2015
+ */
+
+namespace skeeks\cms\modules\admin\models\forms;
+
+use skeeks\cms\models\User;
+use Yii;
+use yii\base\Model;
+
+/**
+ * Class SshConsoleForm
+ * @package skeeks\cms\models\forms
+ */
+class SshConsoleForm extends Model
+{
+    public $command;
+
+    public function attributeLabels()
+    {
+        return [
+            'command' => 'Комманда'
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            // username and password are both required
+            [['command'], 'required'],
+            [['command'], 'string'],
+        ];
+    }
+
+
+
+    /**
+     * Logs in a user using the provided username and password.
+     *
+     * @return boolean whether the user is logged in successfully
+     */
+    public function execute()
+    {
+        if ($this->validate())
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+
+}
