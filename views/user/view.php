db4230a53524ef176eaeadbf2fbbe23c8220b87d
Semenov <semenov@skeeks.com>
2015-03-25 14:32:41 +0300

    Возможность управления страницами через сатовую часть.

--- views/user/view.php
+++ views/user/view.php
@@ -16,222 +16,37 @@ use yii\widgets\ActiveForm;
 /* @var $model common\models\User */
 /* @var $personal bool */
 
-$this->title = 'Личный кабинет пользователя';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = $model->getDisplayName();
+\Yii::$app->breadcrumbs->createBase()->append($this->title);
 ?>
-<div id="itemHeader">
-    <a href="#" id="itemPhoto">
-        <img src="<?= $model->getMainImage(); ?>" alt="">
-    </a>
-    <section id="itemInfo">
-        <header id="itemTitle">
-            <h1><?= $model->getDisplayName(); ?></h1>
-        </header>
-        <div id="itemRating">
-            <!-- Adm rating -->
-            <div class="rating">
-                <h4>«Ничто не истинно. И все дозволено»</h4>
-            </div>
 
+
+<div class="row">
+
+    <aside id="sidebar-right" class="col-md-3">
+		<div id="column-right" class="hidden-xs sidebar">
+            <h1 class="heading_title"><span><?= $model->getDisplayName(); ?></span></h1>
         </div>
-    </section>
+    </aside>
 
-    <div id="itemFollow">
-        <!--<a href="#" class="btn crimson like">Мне нравится</a>-->
+   <section id="sidebar-main" class="col-md-6">
 
+        <div id="content">
+            <h1 class="heading_title"><span><?= $model->name; ?></span></h1>
+                <div class="category-info clearfix">
+                    <div class="category-description wrapper">
 
-        <? if (!$personal) : ?>
-            <? echo \frontend\widgets\Subscribe::widget([
-                "model" => $model
-            ])?>
-        <? else: ?>
-            <a href="#" class="btn darkblue" onclick="return false;">Редактировать страницу</a>
-            <?= Html::a("Выход", Yii::$app->urlManager->createUrl("site/logout"), [
-                "class" => "btn",
-                'data-method' => 'post'
-            ]); ?>
-        <? endif; ?>
-    </div>
-
-
-    <figure id="itemBg">
-        <img src="<?= $model->getImageCover(); ?>" class="popup" alt="" title="">
-        <figcaption>Assassin's Creed</figcaption>
-    </figure>
-
-</div><!-- /itemHeader -->
-
-
-<section id="contentBox">
-    <div id="main" class="eh">
-        <div id="main" class="eh">
-
-
-
-
-            <main class="mainContent1">
-                <ul class="tags1">
-                    <li><a href="#" class="tags-a">Все публикации</a></li>
-                    <li><a href="#">Новости</a></li>
-                    <li><a href="#">Фото</a></li>
-                    <li><a href="#">Видео</a></li>
-                    <li><a href="#">Отзывы</a></li>
-                </ul>
-            </main>
-
-            <?php $form = ActiveForm::begin([
-                "options" => ["class" => "post-comment"],
-                "action" => Yii::$app->urlManager->createUrl(["publication/add"]),
-            ]); ?>
-                <div class="ub">
-                    <a href="<?= \Yii::$app->cms->getAuthUser() ? \Yii::$app->cms->getAuthUser()->getPageUrl() : "#" ?>" class="uph-img">
-                        <img src="<?= \Yii::$app->cms->getAuthUser() ? \Yii::$app->cms->getAuthUser()->getMainImage() : \Yii::$app->params["noimage"] ?>" alt="<?= \Yii::$app->cms->getAuthUser() ? \Yii::$app->cms->getAuthUser()->getDisplayName() : "" ?>">
-                    </a>
-                </div>
-                <div class="comment-input">
-                    <div class="tawrap">
-                        <?= Html::input("hidden", "linked_to_model", $model->getRef()->getCode()); ?>
-                        <?= Html::input("hidden", "linked_to_value", $model->getRef()->getValue()); ?>
-                        <textarea name="content" placeholder="Что у вас нового?"></textarea>
                     </div>
-                    <input type="submit" value="Отправить">
-                    <span class="comment-span"><a href="#">Прикрепить</a></span>
                 </div>
-            <?php $form = ActiveForm::end(); ?>
 
-            <div class="wrapp-main">
-                <? if ($publications = $model->getPublications()->orderBy("created_at DESC")->all()): ?>
-                    <?= \Yii::$app->cms->renderFrontend("widgets/publication/list.php", [
-                        "publications" => $publications
-                    ])?>
-                <? endif; ?>
-
-            </div>
         </div>
-        <aside id="sidebar" class="eh" style="width:38%;">
+   </section>
 
-            <!-- Game info -->
-            <div class="module module1">
-                <h3>Информация о пользователе</h3>
-
-                <ul class="plist">
-                    <li>
-                        <h5 class="plist-h5">Имя</h5>
-                        <?= $model->name; ?>
-                    </li>
-
-                    <li>
-                        <h5 class="plist-h5">Логин</h5>
-                        <?= $model->username; ?>
-                    </li>
-
-                    <li>
-                        <h5 class="plist-h5">Добавил игры</h5>
-                        <?= count($model->getGames()->all()); ?>
-                    </li>
-
-                    <li>
-                        <h5 class="plist-h5">Добавил жанры</h5>
-                        <?= count($model->getGameGenres()->all()); ?>
-                    </li>
+</div>
+ <!--
+  $ospans: allow overrides width of columns base on thiers indexs. format array( column-index=>span number ), example array( 1=> 3 )[value from 1->12]
+ -->
 
 
-                    <li>
-                        <h5 class="plist-h5">Добавил компании</h5>
-                        <?= count($model->getGameCompanies()->all()); ?>
-                    </li>
-
-                    <li>
-                        <h5 class="plist-h5">Добавил платформы</h5>
-                        <?= count($model->getGameCompanies()->all()); ?>
-                    </li>
-
-                    <li>
-                        <h5 class="plist-h5">Публикации</h5>
-                        <?= count($model->getPublications()->all()); ?>
-                    </li>
-
-                    <li>
-                        <h5 class="plist-h5">Комментарии</h5>
-                        <?= count($model->getComments()->all()); ?>
-                    </li>
 
 
-                    <li>
-                        <h5 class="plist-h5">Загрузил изображений</h5>
-                        <?= count($model->getStorageFiles()->all()); ?>
-                    </li>
-
-                    <li>
-                        <h5 class="plist-h5">Всего голосов</h5>
-                        <?= count($model->getVotes()->all()); ?>
-                    </li>
-
-                    <li>
-                        <h5 class="plist-h5">Всего подписок</h5>
-                        <?= count($model->getSubscribes()->all()); ?>
-                    </li>
-                </ul>
-
-                <? if ($personal) : ?>
-                    <a href="#" class="btn2 btn3">Редактировать</a>
-                <? endif; ?>
-            </div>
-
-            <? $subscribes = $model->getSubscribesModels(\common\models\User::className()); ?>
-
-            <div class="module module1">
-                <? if ($subscribes) : ?>
-                    <? $subscribes = $subscribes->all(); ?>
-
-                <h3 class="frend">Друзья
-                    <a href="#">Все друзья</a>
-                    <span><?= count($subscribes); ?></span>
-                </h3>
-
-                    <ul class="frend">
-                        <? foreach($subscribes as $userFriend) : ?>
-                            <li>
-                                <a href="<?= $userFriend->getPageUrl(); ?>"><img src="<?= $userFriend->getMainImage(); ?>" alt=""></a>
-                                <a href="<?= $userFriend->getPageUrl(); ?>"><?= $userFriend->getDisplayName(); ?></a>
-                            </li>
-                        <? endforeach; ?>
-                    </ul>
-                <? endif; ?>
-            </div>
-
-
-            <? $subscribes = $model->getSubscribesModels(\common\models\Game::className()); ?>
-            <div class="module module1">
-                <? if ($subscribes) : ?>
-                    <? $subscribes = $subscribes->all(); ?>
-
-                <h3 class="frend">Подписан на игры
-                    <a href="#">Все игры</a>
-                    <span><?= count($subscribes); ?></span>
-                </h3>
-
-                    <ul class="frend">
-                        <? foreach($subscribes as $sub) : ?>
-                            <li>
-                                <a href="<?= $sub->getPageUrl(); ?>"><img src="<?= $sub->getMainImage(); ?>" alt=""></a>
-                                <a href="<?= $sub->getPageUrl(); ?>"><?= $sub->name; ?></a>
-                            </li>
-                        <? endforeach; ?>
-                    </ul>
-                <? endif; ?>
-            </div>
-
-        </aside>
-
-    </div>
-</section>
-
-<?= Html::a("Выход", Yii::$app->urlManager->createUrl("site/logout"), [
-    "class" => "loginshow",
-    'data-method' => 'post'
-]); ?>
-
-<?= yii\authclient\widgets\AuthChoice::widget([
-     'baseAuthUrl' => ['site/auth']
-]) ?>
