fb6a1899eb9da5f6f8e76646a1bbc47ba14cb564
Semenov <semenov@skeeks.com>
2015-03-30 17:38:23 +0400

    fix toolbar styles

--- views/cms-toolbar.php
+++ views/cms-toolbar.php
@@ -22,7 +22,7 @@ $clientOptionsJson = \yii\helpers\Json::encode($clientOptions);
         <a href="<?= UrlHelper::construct('')->enableAdmin()->toString(); ?>" title="Перейти в панель администрирования"><span class="label label-info">Администрирование</span></a>
     </div>
 
-    <div class="skeeks-cms-toolbar-block">
+    <div class="skeeks-cms-toolbar-block sx-profile">
         <a href="<?= $urlUserEdit; ?>" onclick="new sx.classes.toolbar.Dialog('<?= $urlUserEdit; ?>'); return false;" title="Это вы, перейти к редактированию свох данных">
             <img height="30" src="<?= \skeeks\cms\helpers\Image::getSrc(\Yii::$app->cms->getAuthUser()->getAvatarSrc()); ?>" style="margin-left: 5px;"/> <span class="label label-info"><?= \Yii::$app->cms->getAuthUser()->getDisplayName(); ?></span></a>
     </div>
