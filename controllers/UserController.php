5aa20e3bc5ae072e6740367bc9dbd6333099c8cd
Semenov <semenov@skeeks.com>
2015-04-14 23:52:35 +0400

    Оптии

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -11,6 +11,7 @@
 namespace skeeks\cms\controllers;
 
 use skeeks\cms\base\Controller;
+use skeeks\cms\models\forms\PasswordChangeForm;
 use skeeks\cms\models\User;
 use Yii;
 use skeeks\cms\models\searchs\User as UserSearch;
@@ -104,6 +105,45 @@ class UserController extends Controller
     {
         $data = $this->_user($username);
         return $this->render('view', $data);
+    }
+
+    /**
+     * @param $username
+     * @return string
+     */
+    public function actionChangePassword($username)
+    {
+        $data = $this->_user($username);
+
+        $modelForm = new PasswordChangeForm([
+            'user' => $model
+        ]);
+
+        if (\Yii::$app->request->isAjax && !\Yii::$app->request->isPjax)
+        {
+            $modelForm->load(\Yii::$app->request->post());
+            \Yii::$app->response->format = Response::FORMAT_JSON;
+            return \skeeks\cms\modules\admin\widgets\ActiveForm::validate($modelForm);
+        }
+
+
+        if ($modelForm->load(\Yii::$app->request->post()) && $modelForm->changePassword())
+        {
+            \Yii::$app->getSession()->setFlash('success', 'Успешно сохранено');
+            return $this->redirect(['change-password', 'id' => $model->id]);
+        } else
+        {
+            if (\Yii::$app->request->isPost)
+            {
+                \Yii::$app->getSession()->setFlash('error', 'Не удалось изменить пароль');
+            }
+
+            return $this->output(\Yii::$app->cms->moduleCms()->renderFile('admin-user/_form-change-password.php', [
+                'model' => $modelForm
+            ]));
+        }
+
+        return $this->render($this->action->id, $data);
     }
 
 }
