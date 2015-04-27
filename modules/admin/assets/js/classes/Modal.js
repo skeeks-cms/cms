bcd48040bcc1f00e3854fca0c97203d424ddf873
sanek <semenov@skeeks.com>
2015-04-28 02:51:49 +0300

    Красивые promt окна

--- modules/admin/assets/js/classes/Modal.js
+++ modules/admin/assets/js/classes/Modal.js
@@ -55,6 +55,8 @@
 
         _render: function()
         {
+            var self = this;
+
             $("<div>", {
                 'id' : this.get('id'),
                 'class' : 'modal fade',
@@ -62,6 +64,10 @@
                 'style' : 'display: none;'
             })
             .appendTo(this.jDialogs());
+
+            this.jFooter = $("<div>", {'class' : 'modal-footer'}).append(
+                '<button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>'
+            );
 
             this.jWrapper()
                 .append(
@@ -78,14 +84,18 @@
                                     this.get('content')
                                 )
                             )
-                            .append(
-                                $("<div>", {'class' : 'modal-footer'}).append(
-                                    '<button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>'
-                                )
-                            )
+                            .append(this.jFooter)
                     )
                 );
 
+
+            this.jWrapper().on('hide.bs.modal', function (e) {
+                self.trigger("afterHide", self);
+            });
+
+            this.jWrapper().on('show.bs.modal', function (e) {
+                self.trigger("afterShow", self);
+            });
         },
 
 
@@ -117,6 +127,96 @@
             return this;
         },
 
+    });
+
+
+    sx.classes.modal.Prompt    = sx.classes.modal._Prompt.extend({
+
+        _init: function()
+        {
+            this.applyParentMethod(sx.classes.modal._Prompt, '_init', []);
+
+        },
+
+        /**
+         * @returns {sx.classes.modal._Promt}
+         */
+        show: function()
+        {
+            var self = this;
+
+            var jInput = $("<input>", {
+                'value': this.get("value", ""),
+                'class': "form-control",
+                'name': "sx-promt-value",
+                'id': "sx-promt-value"
+            });
+
+            var jForm = $("<form>", {
+
+            });
+
+            jForm.on('submit', function()
+            {
+                var val = jInput.val();
+                Dialog.hide();
+
+                if (val)
+                {
+                    self.trigger("yes", val);
+                }
+
+                return false;
+            });
+
+            var jSubmit = $("<button>", {
+                'class': "btn btn-primary",
+                'type': "button"
+            }).append(this.get('submitText', "Отправить"));
+
+            var Dialog = new sx.classes.modal.Dialog({
+                'title' : this.get("text"),
+                'content' : jForm.append(jInput)
+            });
+
+            Dialog.bind('afterHide', function(e, data)
+            {
+                self.trigger("no", self);
+            });
+
+            Dialog.bind('afterShow', function(e, data)
+            {
+                _.delay(function()
+                {
+                    jInput.focus();
+                }, 200);
+
+            });
+
+            jSubmit.on("click", function()
+            {
+                jForm.submit();
+                return false;
+            });
+
+            Dialog.jFooter.prepend(jSubmit);
+
+            Dialog.show();
+
+            /*var result = prompt(this.get("text"), );
+
+            if (result)
+            {
+                this.trigger("yes", result);
+            } else
+            {
+                this.trigger("no", this);
+            }
+
+            this.trigger("closed", this);
+
+            return this;*/
+        },
     });
 
 })(sx, sx.$, sx._);
