02264fe1c18189d2652fb526ca1e147848872c08
sanek <semenov@skeeks.com>
2015-05-28 03:45:42 +0300

    1

---
+++ cmsWidgets/treeMenu/_form.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 27.05.2015
+ */
+/* @var $this yii\web\View */
+use skeeks\cms\modules\admin\widgets\form\ActiveFormUseTab as ActiveForm;
+?>
+<?php $form = ActiveForm::begin(); ?>
+    <?= $form->fieldSet('Отображение'); ?>
+        <?= $form->field($model, 'viewFile')->textInput(); ?>
+    <?= $form->fieldSetEnd(); ?>
+
+    <?= $form->fieldSet('Фильтрация'); ?>
+        <?= $form->fieldSelect($model, 'active', \Yii::$app->cms->booleanFormat()); ?>
+        <?= $form->fieldInputInt($model, 'level'); ?>
+        <?= $form->fieldSelect($model, 'site', \yii\helpers\ArrayHelper::map(
+            \skeeks\cms\models\CmsSite::find()->active()->all(),
+            'code',
+            'name'
+        )); ?>
+        <?= $form->field($model, 'treePid')->widget(
+            \skeeks\cms\widgets\formInputs\selectTree\SelectTree::className(),
+            [
+                'mode' => \skeeks\cms\widgets\formInputs\selectTree\SelectTree::MOD_SINGLE,
+                'attributeSingle' => 'treePid'
+            ]
+        ); ?>
+    <?= $form->fieldSetEnd(); ?>
+
+    <?= $form->fieldSet('Сортировка'); ?>
+        <?= $form->fieldSelect($model, 'orderBy', (new \skeeks\cms\models\Tree())->attributeLabels()); ?>
+        <?= $form->fieldSelect($model, 'order', [
+            SORT_ASC    => "ASC (от меньшего к большему)",
+            SORT_DESC   => "DESC (от большего к меньшему)",
+        ]); ?>
+    <?= $form->fieldSetEnd(); ?>
+
+    <?= $form->fieldSet('Дополнительно'); ?>
+        <?= $form->field($model, 'label')->textInput(); ?>
+    <?= $form->fieldSetEnd(); ?>
+
+
+
+<?= $form->buttonsStandart($model) ?>
+<?php ActiveForm::end(); ?>
