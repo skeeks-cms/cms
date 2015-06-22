04d0824b4a8f15b5f5ebff0354a02b850e236052
sanek <semenov@skeeks.com>
2015-06-22 23:27:59 +0300

    Проверка модификации ядра и библиотек

---
+++ checks/StatusVendorsCheck.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 22.06.2015
+ */
+namespace skeeks\cms\checks;
+use skeeks\cms\base\CheckComponent;
+use skeeks\cms\console\controllers\CheckController;
+use skeeks\sx\String;
+
+/**
+ * Class StatusVendorsCheck
+ * @package skeeks\cms\checks
+ */
+class StatusVendorsCheck extends CheckComponent
+{
+    public function init()
+    {
+        $vendorDir              = VENDOR_DIR;
+
+        $this->name             = "Проверка модификации ядра и библиотек";
+        $this->description      = <<<HTML
+<p>
+Осуществаляется проверка, изменения ядра cms и сторонних библиотек (Папка /vendor). Расположение папки и ее название задаются глобальной константой VENDOR_DIR.
+Для текущего проекта:
+</p>
+<p>
+<code>{$vendorDir}</code>
+</p>
+<p>
+Мы настоятельно не рекоммендуем модифицировать ядро проекта, поскольку это может привезти к ошибкам обновления, или же ваши модификации будут удалены в процессе обновления.
+Что в свою очередь, может привести к ошибкам работы проекта.
+</p>
+HTML;
+;
+        $this->errorText    = "Найдены модификации ядра";
+        $this->successText  = "Ядро не модифицировалось";
+
+        parent::init();
+    }
+
+
+    public function run()
+    {
+		$emptyCheck = new EmptyConsoleCheck();
+        $emptyCheck->run();
+
+        if (!$emptyCheck->isSuccess())
+        {
+            $this->addError('Найдены ошибки в процессе работы консольных комманд, проверка модификации ядра не может быть запущена.');
+            return;
+        }
+
+        ob_start();
+            system('cd '  . ROOT_DIR . '; COMPOSER_HOME=.composer php composer.phar status --verbose');
+        $result = ob_get_clean();
+        $result = trim($result);
+
+        if ($result)
+        {
+            $this->addError('Найдены модификации ядра: ' . <<<HTML
+<pre><code>$result</code></pre>
+HTML
+);
+        }
+
+    }
+
+}
