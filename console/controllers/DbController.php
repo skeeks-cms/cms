363eb8023d6209e493fd1e9e2b84355601427810
Semenov <semenov@skeeks.com>
2015-06-26 14:06:20 +0400

    Обновление консольных комманд. Разделение по логике, по контроллерам.

--- console/controllers/DbController.php
+++ console/controllers/DbController.php
@@ -50,4 +50,91 @@ class DbController extends Controller
     {
         \Yii::$app->dbDump->dumpRestore($fileName);
     }
+
+    /**
+     * Инвалидация кэша стуктуры базы данных
+     */
+    public function actionDbRefresh()
+    {
+        \Yii::$app->db->getSchema()->refresh();
+    }
+
+
+    /**
+     * Проведение всех миграций всех подключенных расширений
+     */
+    public function actionApplyMigrations()
+    {
+        $cmd = "php yii migrate --migrationPath=@skeeks/cms/migrations --interactive=0" ;
+        $this->systemCmdRoot($cmd);
+
+        if ($dirs = $this->_findMigrationDirs())
+        {
+            foreach ($dirs as $path)
+            {
+                $cmd = "php yii migrate --migrationPath=" . $path . '  --interactive=0' ;
+                $this->systemCmdRoot($cmd);
+            }
+        }
+
+        /*$this->systemCmdRoot("php yii migrate --interactive=0");*/
+    }
+
+    /**
+     * Найти все дирриктории проекта, и всех расширений, где есть файлы миграций
+     */
+    public function actionFindMigrationDirs()
+    {
+        print_r($this->_findMigrationDirs());
+    }
+
+    /**
+     * Найти все дирриктории проекта, и всех расширений, где могут быть файлы
+     */
+    public function actionFindMigrationPossibleDirs()
+    {
+        print_r($this->_findMigrationPossibleDirs());
+    }
+
+    /**
+     * @return array
+     */
+    private function _findMigrationDirs()
+    {
+        $result = [];
+
+        foreach ($this->_findMigrationPossibleDirs() as $migrationPath)
+        {
+            if (is_dir($migrationPath))
+            {
+                $result[] = $migrationPath;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    private function _findMigrationPossibleDirs()
+    {
+        $result = [];
+
+        foreach (\Yii::$app->extensions as $code => $data)
+        {
+            if ($data['alias'])
+            {
+                foreach ($data['alias'] as $code => $path)
+                {
+                    $migrationsPath = $path . '/migrations';
+                    $result[] = $migrationsPath;
+
+
+                }
+            }
+        }
+
+        return $result;
+    }
 }
