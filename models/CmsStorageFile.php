8810f433a28c01759c95837c6f27d735dc6705bf
Semenov <semenov@skeeks.com>
2015-09-19 19:11:49 +0300

    Добавлена модель CmsStorageFile   * Удалены некоторые старые виджеты работы с формами

---
+++ models/CmsStorageFile.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 19.09.2015
+ */
+namespace skeeks\cms\models;
+/**
+ * Class CmsStorageFile
+ * @package skeeks\cms\models
+ */
+class CmsStorageFile extends StorageFile
+{}
