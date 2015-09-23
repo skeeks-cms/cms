a2bfd2aa1781fa00c4c7390f679e01f82a877c87
Semenov <semenov@skeeks.com>
2015-09-23 21:03:40 +0300

    Исправление привязки элементов к дереву

--- grid/ImageColumn.php
+++ grid/ImageColumn.php
@@ -14,24 +14,5 @@ namespace skeeks\cms\grid;
  * Class ImageColumn
  * @package skeeks\cms\grid
  */
-class ImageColumn extends ImageColumnData
-{
-
-    /**
-     * @inheritdoc
-     */
-    protected function renderDataCellContent($model, $key, $index)
-    {
-        $originalSrc = $model->getMainImageSrc();
-        $src = $model->getPreviewMainImageSrc();
-        if (!$src)
-        {
-            $src = \Yii::$app->cms->moduleAdmin()->noImage;
-            $originalSrc = $src;
-        }
-
-        return "<a href='" . $originalSrc . "' class='sx-fancybox sx-img-link-hover' title='Увеличить' data-pjax='0'>
-                    <img src='" . $src . "' style='width: " . $this->maxWidth . "px;' />
-                </a>";
-    }
-}
+class ImageColumn extends ImageColumn2
+{}
