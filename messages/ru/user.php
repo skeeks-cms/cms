32660055b8aff80cc0e0399a170016df9537d10b
Semenov <semenov@skeeks.com>
2015-10-29 17:58:35 +0300

    Добавлены новые группы переводов в модуле cms

---
+++ messages/ru/user.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Группа переводов для пользователей
+ *
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 29.10.2015
+ */
+return
+[
+    'Name' => 'Имя'
+];
