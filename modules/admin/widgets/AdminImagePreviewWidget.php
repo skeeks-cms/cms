5385b81db98dd750be043e04c698bbd36a16dc0f
Саша <semenov@skeeks.com>
2016-02-05 00:31:03 +0300

    Докстринг

--- modules/admin/widgets/AdminImagePreviewWidget.php
+++ modules/admin/widgets/AdminImagePreviewWidget.php
@@ -19,6 +19,9 @@ class AdminImagePreviewWidget extends Widget
      */
     public $image = null;
 
+    /**
+     * @var string
+     */
     public $maxWidth        = "50px";
 
     public function run()
