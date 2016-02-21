ffeedf642330edafa3c3a55b36303ec4ea7018ee
Саша <semenov@skeeks.com>
2016-02-21 21:57:39 +0300

    Fixed max-width for images

--- views/admin-storage-files/index.php
+++ views/admin-storage-files/index.php
@@ -53,7 +53,7 @@ JS
 
                     $smallImage = \Yii::$app->imaging->getImagingUrl($model->src, new \skeeks\cms\components\imaging\filters\Thumbnail());
                     return "<a href='" . $model->src . "' class='sx-fancybox' data-pjax='0' title='" . \Yii::t('app','Increase') ."'>
-                            <img src='" . $smallImage . "' />
+                            <img src='" . $smallImage . "' style='max-width: 50px;'/>
                         </a>";
                 }
 
