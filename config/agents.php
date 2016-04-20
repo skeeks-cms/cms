16b774affb0d24ef344436a58bf791a01b05391c
Semenov <semenov@skeeks.com>
2016-04-20 21:16:15 +0300

    Rewrote the logic configuration files download

--- config/agents.php
+++ config/agents.php
@@ -1,28 +1,8 @@
 <?php
 return [
-
-    'cms/db/refresh' =>
+    'cms/cache/flush-all' =>
     [
-        'description'       => 'Инвалидация кэша структуры таблиц',
-        'agent_interval'    => 3600*3, //раз в три часа
-        'next_exec_at'      => \Yii::$app->formatter->asTimestamp(time()) + 3600*3,
-        'is_period'         => "N"
-    ],
-
-    'cms/cache/flush-runtimes' =>
-    [
-        'description'       => 'Чистка временных диррикторий',
+        'description'       => 'Чистка кэша',
         'agent_interval'    => 3600*24,
-        'next_exec_at'      => \Yii::$app->formatter->asTimestamp(time()) + 3600*24,
-        'is_period'         => "N"
     ],
-
-    'cms/db/dump' =>
-    [
-        'description'       => 'Бэкап базы данных',
-        'agent_interval'    => 3600*24, //раз в три часа
-        'next_exec_at'      => \Yii::$app->formatter->asTimestamp(time()) + 3600*24,
-        'is_period'         => "N"
-    ]
-
 ];
