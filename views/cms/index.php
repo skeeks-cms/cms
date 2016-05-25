3d9595284d151ded1f76bf8de26bd9ccd6806468
Semenov <semenov@skeeks.com>
2016-05-25 22:29:11 +0300

    Loading forms with default values

--- views/cms/index.php
+++ views/cms/index.php
@@ -5,6 +5,8 @@
  * @copyright 2010 SkeekS (СкикС)
  * @date 13.04.2016
  */
+/* @var $this yii\web\View */
+$this->title = 'Система управления сайтом: SkeekS CMS (Yii2)';
 ?>
 
 <div style="text-align: center; padding: 100px;">
