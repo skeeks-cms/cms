be3d215bb09acf7719672edc39a96fb2ceaf9159
Саша <semenov@skeeks.com>
2016-07-28 22:43:16 +0300

    Fixed bug

--- models/CmsContentPropertyEnum.php
+++ models/CmsContentPropertyEnum.php
@@ -11,8 +11,6 @@ namespace skeeks\cms\models;
 
 use skeeks\cms\base\Widget;
 use skeeks\cms\helpers\UrlHelper;
-use skeeks\cms\models\behaviors\HasMultiLangAndSiteFields;
-use skeeks\cms\models\behaviors\HasStatus;
 use skeeks\cms\models\behaviors\TimestampPublishedBehavior;
 use skeeks\cms\relatedProperties\models\RelatedPropertyEnumModel;
 use Yii;
