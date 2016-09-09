5fdaf638e73e828affddd45cfcea9da4616a3e15
Semenov <semenov@skeeks.com>
2016-09-09 18:51:10 +0300

    Changing the mechanism of additional properties \Yii::$app->cms->relatedHandlers

--- views/admin-tools/select-cms-element.php
+++ views/admin-tools/select-cms-element.php
@@ -132,7 +132,7 @@ JS
 
                 if ($property->property_type == \skeeks\cms\relatedProperties\PropertyType::CODE_ELEMENT)
                 {
-                    $propertyType = $property->createPropertyType();
+                    $propertyType = $property->handler;
                         $options = \skeeks\cms\models\CmsContentElement::find()->active()->andWhere([
                             'content_id' => $propertyType->content_id
                         ])->all();
