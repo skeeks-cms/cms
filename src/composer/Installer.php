16c7f2da751c85c92d4db7d89e10a683abe65bc9
Саша <semenov@skeeks.com>
2017-08-10 10:38:51 +0300

    Composer installer update

--- src/composer/Installer.php
+++ src/composer/Installer.php
@@ -6,6 +6,7 @@ use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 use skeeks\cms\components\Cms;
 use skeeks\cms\helpers\FileHelper;
+use yii\helpers\ArrayHelper;
 
 class Installer
 {
@@ -21,6 +22,24 @@ class Installer
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
         echo "\tpostUpdate\n";
         self::generateTmpConfigs($vendorDir);
+    }
+
+    public static function clearDirs(Event $event)
+    {
+        $params = $event->getComposer()->getPackage()->getExtra();
+
+        if (isset($params[__METHOD__]['dirs']) && is_array($params[__METHOD__]['dirs'])) {
+            foreach ($params[__METHOD__]['dirs'] as $dir)
+            {
+                if (is_dir($dir))
+                {
+                    $dir = realpath($dir);
+                    echo "\tclear dir: {$dir}\n";
+                    FileHelper::removeDirectory($dir);
+                    FileHelper::createDirectory($dir);
+                }
+            }
+        }
     }
 
     static public function generateTmpConfigs($vendorDir)
