3cc9e0e8d9f37a2d54efca78003cb36db18b96e9
semenov <semenov@skeeks.com>
2017-11-14 18:47:50 +0300

    Use skeeks/cms-toolbar

--- src/config/web.php
+++ src/config/web.php
@@ -64,6 +64,23 @@ $config =
         [
             'class' => '\skeeks\cms\components\Breadcrumbs',
         ],
+
+        'cmsAgent' =>
+        [
+            'commands' => [
+                'cms/cache/flush-all' =>
+                [
+                    'description'       => 'Чистка кэша',
+                    'agent_interval'    => 3600*24,
+                ],
+
+                'ajaxfileupload/cleanup' =>
+                [
+                    'description'       => 'Чистка временно загружаемых файлов',
+                    'agent_interval'    => 3600*24,
+                ],
+            ]
+        ],
     ],
 
 
