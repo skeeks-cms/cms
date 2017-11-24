1719ab2834e508a91b6ad00029b1fc1ceb5458a2
Саша <semenov@skeeks.com>
2017-11-24 09:10:23 +0300

    Code style

--- src/grid/ComponentSettingsColumn.php
+++ src/grid/ComponentSettingsColumn.php
@@ -49,7 +49,7 @@ class ComponentSettingsColumn extends BooleanColumn
             return $this->_result(Cms::BOOL_Y);
         }
 
-        return $this->_result(Cms::BOOL_N);;
+        return $this->_result(Cms::BOOL_N);
     }
 
     /**
