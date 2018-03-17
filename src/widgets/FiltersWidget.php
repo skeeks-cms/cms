806958808249e8bc26555bd6f3e0645705c2d2c0
Semenov <semenov@skeeks.com>
2018-03-17 19:07:27 +0300

    Change children elements logic

--- src/widgets/FiltersWidget.php
+++ src/widgets/FiltersWidget.php
@@ -13,10 +13,13 @@ use skeeks\cms\IHasModel;
 use skeeks\yii2\config\ConfigBehavior;
 use skeeks\yii2\config\DynamicConfigModel;
 use skeeks\yii2\form\fields\SelectField;
+use skeeks\yii2\form\fields\TextField;
+use yii\base\Model;
 use yii\base\Widget;
 use yii\data\ActiveDataProvider;
 use yii\data\DataProviderInterface;
 use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
 
 /**
  * @property string                $modelClassName; название класса модели с которой идет работа
@@ -45,10 +48,21 @@ class FiltersWidget extends Widget
     public $config = [];
 
     /**
+     * @var bool генерировать фильтры автоматически
+     */
+    public $isEnabledAutoFilters = true;
+
+    /**
      * @var IHasModel|array|DynamicConfigModel
      */
     public $filtersModel;
 
+    /**
+     * @var array
+     */
+    public $activeForm = [
+        'class' => ActiveForm::class
+    ];
 
     public function behaviors()
     {
@@ -97,6 +111,11 @@ class FiltersWidget extends Widget
            'class' => DynamicConfigModel::class,
         ];
 
+        //Автомтическое конфигурирование колонок
+        $this->_initAutoFilters();
+
+        $defaultFiltersModel = ArrayHelper::merge((array) $this->_autoDynamicModelData, $defaultFiltersModel);
+
         $this->filtersModel = ArrayHelper::merge($defaultFiltersModel, (array) $this->filtersModel);
         $this->filtersModel = \Yii::createObject($this->filtersModel);
         $this->filtersModel->load(\Yii::$app->request->get());
@@ -108,17 +127,27 @@ class FiltersWidget extends Widget
             }
         }
 
-        $this->trigger(self::EVENT_INIT);
+        $defaultActiveForm = [
+            'action' => [''],
+            'method' => 'get',
+            //'layout' => 'horizontal',
+            'class' => ActiveForm::class,
+        ];
+
+        $this->activeForm = ArrayHelper::merge($defaultActiveForm, $this->activeForm);
+
+        parent::init();
+
+        //Применение включенных/выключенных фильтров
+        $this->_applyFilters();
     }
 
     public function run()
     {
-        $form = \yii\bootstrap\ActiveForm::begin([
-            'action' => [''],
-            'method' => 'get',
-            'layout' => 'horizontal',
-        ]);
+        $activeFormClassName = ArrayHelper::getValue($this->activeForm, 'class', ActiveForm::class);
+        ArrayHelper::remove($this->activeForm, 'class');
 
+        $form = $activeFormClassName::begin((array) $this->activeForm);
 
         echo (new \skeeks\yii2\form\Builder([
             'models'     => $this->filtersModel->builderModels(),
@@ -127,8 +156,7 @@ class FiltersWidget extends Widget
             'fields'     => $this->filtersModel->builderFields(),
         ]))->render();
 
-
-        \yii\bootstrap\ActiveForm::end();
+        $activeFormClassName::end();
     }
 
 
@@ -150,6 +178,78 @@ class FiltersWidget extends Widget
             /*$result = ArrayHelper::merge($result, $this->_resultColumns);
             $this->_resultColumns = $result;*/
             $this->columns = $result;
+        }
+
+        return $this;
+    }
+
+
+
+
+    private $_autoDynamicModelData = [];
+
+    /**
+     * This function tries to guess the columns to show from the given data
+     * if [[columns]] are not explicitly specified.
+     */
+    protected function _initAutoFilters()
+    {
+        //Если автоопределение колонок не включено
+        if (!$this->isEnabledAutoFilters) {
+            return $this;
+        }
+
+        $dataProvider = clone $this->dataProvider;
+        $models = $dataProvider->getModels();
+
+
+        /**
+         * @var $model Model
+         */
+        $model = reset($models);
+
+        $result = [];
+
+        $result['attributeDefines'] = $model->attributes();
+        $result['attributeLabels'] = $model->attributeLabels();
+
+        $rules = [];
+        $fields = [];
+
+        if (is_array($model) || is_object($model)) {
+            foreach ($model as $name => $value) {
+                if ($value === null || is_scalar($value) || is_callable([$value, '__toString'])) {
+                    $fields[(string)$name] = [
+                        'class' => TextField::class,
+                    ];
+
+                    $rules[] = [(string)$name, 'safe'];
+                }
+            }
+        }
+
+        $result['rules'] = $rules;
+        $result['fields'] = $fields;
+        $this->_autoDynamicModelData = $result;
+
+        return $this;
+    }
+
+    protected function _applyFilters()
+    {
+        $result = [];
+        $fields = $this->filtersModel->builderFields();
+
+        //Есть логика включенных выключенных колонок
+        if ($this->visibleFilters && $fields) {
+
+            foreach ($this->visibleFilters as $key) {
+                $result[$key] = ArrayHelper::getValue($fields, $key);
+            }
+        }
+
+        if ($result) {
+            $this->filtersModel->setFields($result);
         }
 
         return $this;
