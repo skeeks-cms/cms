8c410c0a932b7472b34b1577e56c4c0161407322
Semenov <semenov@skeeks.com>
2018-05-06 13:12:57 +0300

    dev

--- src/controllers/AdminCmsSiteTwoController.php
+++ src/controllers/AdminCmsSiteTwoController.php
@@ -8,6 +8,9 @@
 
 namespace skeeks\cms\controllers;
 
+use skeeks\cms\backend\actions\BackendModelDeleteAction;
+use skeeks\cms\backend\actions\BackendModelMultiAction;
+use skeeks\cms\backend\actions\BackendModelMultiDeleteAction;
 use skeeks\cms\backend\controllers\BackendModelStandartController;
 use skeeks\cms\components\Cms;
 use skeeks\cms\grid\BooleanColumn;
@@ -17,7 +20,12 @@ use skeeks\cms\modules\admin\actions\modelEditor\AdminMultiModelEditAction;
 use skeeks\cms\modules\admin\traits\AdminModelEditorStandartControllerTrait;
 use skeeks\cms\queryfilters\filters\modes\FilterModeEmpty;
 use skeeks\cms\queryfilters\filters\modes\FilterModeNotEmpty;
+use skeeks\yii2\form\fields\BoolField;
+use skeeks\yii2\form\fields\FieldSet;
 use skeeks\yii2\form\fields\SelectField;
+use skeeks\yii2\form\fields\TextareaField;
+use skeeks\yii2\form\fields\TextField;
+use skeeks\yii2\form\fields\WidgetField;
 use yii\helpers\ArrayHelper;
 
 /**
@@ -52,6 +60,45 @@ class AdminCmsSiteTwoController extends BackendModelStandartController
                 ],
             ],
         ];
+
+        $updateFields = [
+            'main' => [
+                'class' => FieldSet::class,
+                'name' => \Yii::t('skeeks/cms', 'Main'),
+                'fields' => [
+                    'image_id' => [
+                        'class' => WidgetField::class,
+                        'widgetClass' => \skeeks\cms\widgets\AjaxFileUploadWidget::class,
+                        'widgetConfig' => [
+                            'accept' => 'image/*',
+                            'multiple' => false
+                        ]
+                    ],
+                    'name',
+                    'code',
+                    'active' => [
+                        'class' => BoolField::class,
+                        'formElement' => BoolField::ELEMENT_RADIO_LIST,
+                        'allowNull' => false,
+                        'trueValue' => 'Y',
+                        'falseValue' => 'N',
+                    ],
+                    'def' => [
+                        'class' => BoolField::class,
+                        'formElement' => BoolField::ELEMENT_RADIO_LIST,
+                        'allowNull' => false,
+                        'trueValue' => 'Y',
+                        'falseValue' => 'N',
+                    ],
+                    'description' => [
+                        'class' => TextareaField::class
+                    ],
+                    'server_name',
+                    'priority',
+                ]
+            ],
+        ];
+
         return ArrayHelper::merge(parent::actions(), [
 
                 'index' => [
@@ -109,24 +156,24 @@ class AdminCmsSiteTwoController extends BackendModelStandartController
                     ],
                 ],
 
-
-                "def-multi" => [
-                    'class'        => AdminMultiModelEditAction::className(),
-                    "name"         => "По умолчанию",
-                    //"icon"              => "glyphicon glyphicon-trash",
-                    "eachCallback" => [$this, 'eachMultiDef'],
-                    "priority"     => 0,
+                "create" => [
+                    'fields' => $updateFields
                 ],
 
+                "update" => [
+                    'fields' => $updateFields
+                ],
+
+
                 "activate-multi" => [
-                    'class'        => AdminMultiModelEditAction::className(),
+                    'class'        => BackendModelMultiAction::class,
                     "name"         => "Активировать",
                     //"icon"              => "glyphicon glyphicon-trash",
                     "eachCallback" => [$this, 'eachMultiActivate'],
                 ],
 
                 "inActivate-multi" => [
-                    'class'        => AdminMultiModelEditAction::className(),
+                    'class'        => BackendModelMultiAction::class,
                     "name"         => "Деактивировать",
                     //"icon"              => "glyphicon glyphicon-trash",
                     "eachCallback" => [$this, 'eachMultiInActivate'],
