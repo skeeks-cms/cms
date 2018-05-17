c281787796f77462be1d80bb8ae44a80e6e54284
Aleksandr_Semenov <semenov@skeeks.com>
2018-05-17 13:52:07 +0300

    remove old

--- src/i18n/I18N.php
+++ src/i18n/I18N.php
@@ -21,19 +21,22 @@ class I18N extends \yii\i18n\I18N
      */
     public function init()
     {
-        /*if (!isset($this->translations['*'])) {
-            $this->translations['*'] = [
-                'class' => 'yii\i18n\PhpMessageSource'
-            ];
-        }*/
+        if ($this->translations) {
+            foreach ($this->translations as $key => $data)
+            {
+                if (!isset($data['on missingTranslation'])) {
+                    $data['on missingTranslation'] = $this->missingTranslationHandler;
+                    $this->translations[$key] = $data;
+                }
+            }
+        }
 
         parent::init();
     }
 
     public static function handleMissingTranslation(MissingTranslationEvent $event)
     {
-        \Yii::info("@MISSING: {$event->category}.{$event->message} FOR LANGUAGE {$event->language} @",
-            self::className());
+        \Yii::warning("@MISSING: {$event->category}.{$event->message} FOR LANGUAGE {$event->language} @", self::class);
 
         if ($event->category != 'skeeks/cms') {
             $event->translatedMessage = \Yii::t('skeeks/cms', $event->message);
