5bffd597acdfe84212f3f95037504bca863067cf
semenov <semenov@skeeks.com>
2018-12-29 16:42:36 +0300

    Fiexed pjax

--- src/controllers/OnlineController.php
+++ src/controllers/OnlineController.php
@@ -10,6 +10,7 @@ namespace skeeks\cms\controllers;
 
 use skeeks\cms\actions\user\UserAction;
 use skeeks\cms\helpers\RequestResponse;
+use yii\helpers\Json;
 use yii\web\Controller;
 
 /**
@@ -22,9 +23,14 @@ class OnlineController extends Controller
      */
     public function actionTrigger()
     {
+        $callback = \Yii::$app->request->get('callback');
+
         $rr = new RequestResponse();
+        $rr->data['call'] = \Yii::$app->request->get('callback');
         $rr->success = true;
-        return $rr;
+
+        $data = Json::encode($rr->toArray());
+        return "{$callback}({$data})";
     }
 
 }
