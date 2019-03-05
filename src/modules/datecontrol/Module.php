97525dc990057bd707b21f78ac7b2066aa3ea708
semenov <semenov@skeeks.com>
2019-03-05 15:14:43 +0300

    dev

--- src/modules/datecontrol/Module.php
+++ src/modules/datecontrol/Module.php
@@ -18,18 +18,17 @@ class Module extends \kartik\datecontrol\Module
     public $controllerNamespace = 'kartik\datecontrol\controllers';
 
     public $displaySettings = [
-        \kartik\datecontrol\Module::FORMAT_DATE => 'dd-MM-yyyy',
-        \kartik\datecontrol\Module::FORMAT_TIME => 'HH:mm:ss',
+        \kartik\datecontrol\Module::FORMAT_DATE     => 'dd-MM-yyyy',
+        \kartik\datecontrol\Module::FORMAT_TIME     => 'HH:mm:ss',
         \kartik\datecontrol\Module::FORMAT_DATETIME => 'dd-MM-yyyy HH:mm:ss',
     ];
 
     // format settings for saving each date attribute (PHP format example)
-    public $saveSettings =
-        [
-            \kartik\datecontrol\Module::FORMAT_DATE => 'php:U', // saves as unix timestamp
-            \kartik\datecontrol\Module::FORMAT_TIME => 'php:U', //'php:H:i:s',
-            \kartik\datecontrol\Module::FORMAT_DATETIME => 'php:U', //'php:Y-m-d H:i:s',
-        ];
+    public $saveSettings = [
+        \kartik\datecontrol\Module::FORMAT_DATE     => 'php:U', // saves as unix timestamp
+        \kartik\datecontrol\Module::FORMAT_TIME     => 'php:U', //'php:H:i:s',
+        \kartik\datecontrol\Module::FORMAT_DATETIME => 'php:U', //'php:Y-m-d H:i:s',
+    ];
 
     // set your display timezone
     public $displayTimezone = 'Europe/Moscow';
@@ -45,36 +44,43 @@ class Module extends \kartik\datecontrol\Module
     public $ajaxConversion = true;
 
     // default settings for each widget from kartik\widgets used when autoWidget is true
-    public $autoWidgetSettings =
-        [
-            \kartik\datecontrol\Module::FORMAT_DATE => ['type' => 2, 'pluginOptions' => ['autoclose' => true]],
-            // example
-            \kartik\datecontrol\Module::FORMAT_DATETIME => [],
-            // setup if needed
-            \kartik\datecontrol\Module::FORMAT_TIME => [],
-            // setup if needed
-        ];
+    public $autoWidgetSettings = [
+        \kartik\datecontrol\Module::FORMAT_DATE     => [
+            'pluginOptions' => [
+                'autoclose' => true,
+            ],
+        ],
+        // example
+        \kartik\datecontrol\Module::FORMAT_DATETIME => [
+            'pluginOptions' => [
+                'autoclose' => true,
+            ],
+        ],
+        // setup if needed
+        \kartik\datecontrol\Module::FORMAT_TIME     => [],
+        // setup if needed
+    ];
 
     // custom widget settings that will be used to render the date input instead of kartik\widgets,
     // this will be used when autoWidget is set to false at module or widget level.
     public $widgetSettings = [
         \kartik\datecontrol\Module::FORMAT_DATE => [
             //'class' => '\yii\jui\DatePicker', // example
-            'class' => '\kartik\datetime\DatePicker',
+            'class'   => '\kartik\datetime\DatePicker',
             'options' => [
                 'dateFormat' => 'php:d-M-Y',
-                'options' => ['class' => 'form-control'],
-            ]
+                'options'    => ['class' => 'form-control'],
+            ],
         ],
 
         \kartik\datecontrol\Module::FORMAT_DATETIME => [
             //'class' => '\yii\jui\DatePicker', // example
-            'class' => '\kartik\datetime\DateTimePicker',
+            'class'   => '\kartik\datetime\DateTimePicker',
             'options' => [
                 'dateFormat' => 'php:d-F-Y H:i:s',
-                'options' => ['class' => 'form-control'],
-            ]
-        ]
+                'options'    => ['class' => 'form-control'],
+            ],
+        ],
     ];
     // other settings
 }
