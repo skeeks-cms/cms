78569ccdf34b43f4c2a930d46e149efc7b9de386
Semenov <semenov@skeeks.com>
2021-04-28 00:09:31 +0300

    dev

---
+++ src/widgets/PjaxLazyLoad.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 19.12.2016
+ */
+
+namespace skeeks\cms\widgets;
+
+/**
+ * @property bool isPjax;
+ *
+ * @package skeeks\cms\widgets
+ */
+class PjaxLazyLoad extends Pjax
+{
+    /**
+     * @var int
+     */
+    public $delay = 1000;
+
+
+    public static $autoIdPrefix = 'PjaxLazyLoad';
+
+    /**
+     * @return bool
+     */
+    public function getIsPjax() {
+        return $this->requiresPjax();
+    }
+
+    /**
+     * @return string|void
+     */
+    public function run()
+    {
+        if (!$this->isPjax) {
+            \Yii::$app->view->registerJs(<<<JS
+    setTimeout(function() {
+        $.pjax.reload("#{$this->id}", {
+            'timeout': $this->timeout,
+            //'push': false,
+        });
+    }, $this->delay);
+JS
+            );
+        }
+
+        parent::run();
+    }
+}
