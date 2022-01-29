9a125a8780be1445a492bebce2f97b49e8501e4f
Semenov <semenov@skeeks.com>
2022-01-29 19:53:53 +0300

    dev

---
+++ src/views/admin-cms-content-element/_model_header.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+/**
+ * @var $this yii\web\View
+ * @var $model \skeeks\cms\models\CmsContentElement
+ */
+?>
+<div class="row" style="margin-bottom: 5px;">
+    <? if ($model->image) : ?>
+        <div class="col my-auto" style="max-width: 60px">
+            <img style="border: 2px solid #ededed; border-radius: 5px;" src="<?php echo \Yii::$app->imaging->getImagingUrl($model->image->src,
+                new \skeeks\cms\components\imaging\filters\Thumbnail()); ?>" />
+        </div>
+    <? endif; ?>
+    <div class="col my-auto">
+        <h1 style="margin-bottom: 0px; line-height: 1.1;"><?php echo $model->name; ?></h1>
+        <div class="sx-small-info" style="font-size: 10px; color: silver;">
+            <span title="ID записи - уникальный код записи в базе данных." data-toggle="tooltip"><i class="fas fa-key"></i> <?php echo $model->id; ?></span>
+            <? if ($model->created_at) : ?>
+                <span style="margin-left: 5px;" data-toggle="tooltip" title="Запись создана в базе: <?php echo \Yii::$app->formatter->asDatetime($model->created_at); ?>"><i class="far fa-clock"></i> <?php echo \Yii::$app->formatter->asDate($model->created_at); ?></span>
+            <? endif; ?>
+            <? if ($model->created_by) : ?>
+                <span style="margin-left: 5px;" data-toggle="tooltip" title="Запись создана пользователем с ID: <?php echo $model->createdBy->id; ?>"><i class="far fa-user"></i> <?php echo $model->createdBy->shortDisplayName; ?></span>
+            <? endif; ?>
+            <? if ($model->tree_id) : ?>
+                <span style="margin-left: 5px;" data-toggle="tooltip" title="<?php echo $model->cmsTree->fullName; ?>"><i class="far fa-folder"></i> <?php echo $model->cmsTree->name; ?></span>
+            <? endif; ?>
+
+        </div>
+    </div>
+    <? if ($model->cmsContent->is_have_page) : ?>
+        <div class="col my-auto" style="max-width: 70px; text-align: right;">
+            <a href="" data-toggle="tooltip" class="btn btn-default" target="_blank" title="<?php echo \Yii::t('skeeks/cms', 'Watch to site (opens new window)'); ?>"><i class="fas fa-external-link-alt"></i></a>
+        </div>
+    <? endif; ?>
+</div>
---
+++ src/views/admin-user/_model_header.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+/**
+ * @var $this yii\web\View
+ * @var $model \skeeks\cms\models\CmsContentElement
+ */
+?>
+<div class="row" style="margin-bottom: 5px;">
+    <? if ($model->image) : ?>
+        <div class="col my-auto" style="max-width: 60px">
+            <img style="border: 2px solid #ededed; border-radius: 50%;" src="<?php echo \Yii::$app->imaging->getImagingUrl($model->image->src,
+                new \skeeks\cms\components\imaging\filters\Thumbnail()); ?>" />
+        </div>
+    <? endif; ?>
+    <div class="col my-auto">
+        <h1 style="margin-bottom: 0px; line-height: 1.1;"><?php echo $model->displayName; ?></h1>
+        <div class="sx-small-info" style="font-size: 10px; color: silver;">
+            <span title="ID записи - уникальный код записи в базе данных." data-toggle="tooltip"><i class="fas fa-key"></i> <?php echo $model->id; ?></span>
+            <? if ($model->created_at) : ?>
+                <span style="margin-left: 5px;" data-toggle="tooltip" title="Запись создана в базе: <?php echo \Yii::$app->formatter->asDatetime($model->created_at); ?>"><i class="far fa-clock"></i> <?php echo \Yii::$app->formatter->asDate($model->created_at); ?></span>
+            <? endif; ?>
+            <? if ($model->created_by) : ?>
+                <span style="margin-left: 5px;" data-toggle="tooltip" title="Запись создана пользователем с ID: <?php echo $model->createdBy->id; ?>"><i class="far fa-user"></i> <?php echo $model->createdBy->shortDisplayName; ?></span>
+            <? endif; ?>
+            <? if ($model->email) : ?>
+                <span style="margin-left: 5px;"><i class="far fa-envelope"></i> <?php echo $model->email; ?></span>
+            <? endif; ?>
+            <? if ($model->phone) : ?>
+                <span style="margin-left: 5px;"><i class="fas fa-phone"></i> <?php echo $model->phone; ?></span>
+            <? endif; ?>
+
+        </div>
+    </div>
+</div>
