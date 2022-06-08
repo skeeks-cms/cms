5da7dd0218e0e894ffb66dfe2b20661cf6e57785
Semenov <semenov@skeeks.com>
2022-06-08 11:01:46 +0300

    big update

---
+++ src/widgets/admin/CmsUserViewWidget.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\cms\widgets\admin;
+
+use common\models\User;
+use skeeks\cms\models\CmsUser;
+use skeeks\crm\models\CrmContractor;
+use yii\base\Widget;
+
+class CmsUserViewWidget extends Widget
+{
+    /**
+     * @var CmsUser
+     */
+    public $cmsUser = null;
+
+    /**
+     * @var bool Показывать только название
+     */
+    public $isShowOnlyName = false;
+
+    /**
+     * @var string
+     */
+    public $tagName = "a";
+
+
+    public $viewFile = "user-view";
+
+    /**
+     * @var string
+     */
+    public $append = "";
+
+    /**
+     * @var array
+     */
+    public $tagNameOptions = [];
+
+    /**
+     * @var int
+     */
+    public $prviewImageSize = 50;
+
+    public function run()
+    {
+        if ($this->cmsUser) {
+            $cache = \Yii::$app->cache->get("cmsUser{$this->cmsUser->id}" . $this->tagName . $this->prviewImageSize);
+            if ($cache) {
+                return $cache;
+            }
+
+            $result = $this->render($this->viewFile);
+            \Yii::$app->cache->set("cmsUser{$this->cmsUser->id}", $result, 2);
+            return $result;
+        } else {
+            return "";
+        }
+
+    }
+}
---
+++ src/widgets/admin/views/user-view.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+/* @var $this yii\web\View */
+/**
+ * @var $cmsUser \skeeks\cms\models\CmsUser
+ */
+$widget = $this->context;
+$cmsUser = $widget->cmsUser;
+
+$class = 'g-brd-gray-light-v4';
+
+?>
+<div class="d-flex flex-row sx-preview-card">
+
+    <div>
+        <?php
+        $w = \skeeks\cms\backend\widgets\AjaxControllerActionsWidget::begin([
+            'controllerId' => '/cms/admin-user',
+            'modelId'      => $cmsUser->id,
+            'content'      => $cmsUser->shortDisplayNameWithAlias,
+            'options'      => [
+                'data-toggle' => 'tooltip',
+                'data-html'   => 'true',
+                'data-pjax'   => '0',
+                'style'       => 'border: 0;',
+            ],
+        ]); ?>
+            <img src="<?= \Yii::$app->imaging->thumbnailUrlOnRequest($cmsUser->image ? $cmsUser->image->src : \skeeks\cms\helpers\Image::getCapSrc(),
+                new \skeeks\cms\components\imaging\filters\Thumbnail([
+                    'h' => $widget->prviewImageSize,
+                    'w' => $widget->prviewImageSize,
+                ])); ?>" alt=""
+                 class="sx-photo <?= $class; ?> sx-img-size-<?= $widget->prviewImageSize; ?>"
+                 data-toggle="tooltip"
+                 data-html="true"
+            >
+        <?php $w::end(); ?>
+    </div>
+
+    <div>
+
+        <?php
+        $options = \yii\helpers\ArrayHelper::merge([
+            'style' => 'text-align: left; white-space: nowrap;',
+            'class' => '',
+            'href'  => '#',
+
+            'data-toggle' => 'tooltip',
+            'data-html'   => 'true',
+            'data-pjax'   => '0',
+        ], (array) $widget->tagNameOptions);
+
+        echo \skeeks\cms\backend\widgets\AjaxControllerActionsWidget::widget([
+            'controllerId' => '/cms/admin-user',
+            'modelId'      => $cmsUser->id,
+            'content'      => $cmsUser->shortDisplayNameWithAlias,
+            'tag'          => $widget->tagName,
+            'options'      => $options,
+        ]); ?>
+
+
+        <? if ($widget->isShowOnlyName === false) : ?>
+
+            <? if ($cmsUser->phone) : ?>
+                <div class="sx-phone">
+                    <a href="tel:<?= $cmsUser->phone; ?>" style="color: gray; font-size: 12px; text-decoration: none; border-bottom: 0px;">
+                        <i class="fas fa-phone"></i> <?= $cmsUser->phone; ?>
+                    </a>
+                </div>
+            <? endif; ?>
+            <? if ($cmsUser->email) : ?>
+                <div class="sx-mail">
+                    <a href="mailto:<?= $cmsUser->email; ?>"  style="color: gray; font-size: 12px; text-decoration: none; border-bottom: 0px;">
+                        <i class="far fa-envelope"></i> <?= $cmsUser->email; ?>
+                    </a>
+                </div>
+            <? endif; ?>
+        <? endif; ?>
+        <? if ($widget->append) : ?>
+            <?php echo $widget->append; ?>
+        <? endif; ?>
+    </div>
+
+
+</div>
+
+
