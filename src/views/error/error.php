72ba84dc2ad81f45bfa13e7e480dda1af087b271
Semenov <semenov@skeeks.com>
2022-11-14 13:11:32 +0300

    dev

--- src/views/error/error.php
+++ src/views/error/error.php
@@ -8,20 +8,31 @@ use yii\helpers\Html;
 /* @var $exception Exception */
 
 $this->title = $name;
+$this->registerCss(<<<CSS
+.sx-error-section  {
+    min-height: 50vh;
+    display: flex;
+    text-align: center;
+}
+.sx-error-section .sx-container {
+    margin: auto;
+}
+.sx-buttons {
+    margin-top: 20px;
+}
+CSS
+);
 ?>
-<div class="site-error">
+<div class="sx-error-section">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+    <div class="container sx-container">
+        <h1><?= \Yii::t('skeeks/cms', 'Ошибка'); ?> <?= isset($exception->statusCode) ? $exception->statusCode : $exception->getCode(); ?></h1>
+        <?= nl2br(Html::encode($message)); ?>
+        <div class="sx-buttons">
+            <?php if(\Yii::$app->request->referrer) : ?>
+                <a href="<?php echo \Yii::$app->request->referrer; ?>" class="btn btn-primary btn-xl"><?= \Yii::t('skeeks/cms', 'Назад'); ?></a>
+            <?php endif; ?>
+            <a href="<?php echo \yii\helpers\Url::home(); ?>" class="btn btn-primary btn-xl"><?= \Yii::t('skeeks/cms', 'Вернуться на главную'); ?></a>
+        </div>
     </div>
-
-    <p>
-        <?= \Yii::t('skeeks/cms', 'The above error occurred while the Web server was processing your request.'); ?>
-    </p>
-    <p>
-        <?= \Yii::t('skeeks/cms', 'Please contact us if you think this is a server error. Thank you.'); ?>
-    </p>
-
 </div>
