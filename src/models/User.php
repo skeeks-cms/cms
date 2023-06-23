51090ab534a9de692dabe48f7e06ae3855ef6e91
Semenov <semenov@skeeks.com>
2023-06-23 15:31:35 +0300

    Обновление

--- src/models/User.php
+++ src/models/User.php
@@ -186,7 +186,7 @@ class User
             foreach ((array)$this->_roleNames as $roleName) {
                 if ($role = \Yii::$app->authManager->getRole($roleName)) {
                     try {
-                        if (!\Yii::$app->authManager->getAssignment($role, $this->id)) {
+                        if (!\Yii::$app->authManager->getAssignment($roleName, $this->id)) {
                             \Yii::$app->authManager->assign($role, $this->id);
                         }
                     } catch (\Exception $e) {
